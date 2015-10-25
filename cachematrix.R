af97be625ff1d2120e6cb1dfa868ec79a0788924
halldj1982 <david.hall.richmond@gmail.com>
2015-10-25 16:46:14 -0400

    Created required functions to initialize a matrix, set its inverse, and store the result

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
+## A function to initialize a matrix and store it in a cached variable and define a set of
+## sub functions to set the matrix, get a copy of the matrix, and create an iverse of the matrix
 ## functions do
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function(){i}
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Function to create an inverse of the passed matrix using the "solve" function. Returns
+## the inverse copy already stored if present.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setInverse(i)
+    i
+  
 }
